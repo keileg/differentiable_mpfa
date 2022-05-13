2fce16e7a47e1668995f139de146c891c58bcf2b
Ivar Stefansson <ivar.stefansson@uib.no>
2022-05-13 16:26:36 +0200

    Plotting and archiving

--- archive.py
+++ archive.py
@@ -1,7 +1,9 @@
+from typing import Dict, List
+
 import numpy as np
 import porepy as pp
 
-from models import BiotNonlinearTpfa
+from models import BiotNonlinearTpfa, Chemistry, NonlinearIncompressibleFlow
 from run_chemistry import ChemistryParameters
 from run_verification import SquareRootPermeability
 
@@ -126,3 +128,109 @@ class SquarePermeability2d(SquareRootPermeability):
     def _permeability_function_ad(self, pressure: pp.ad.Variable):
         val = pressure ** 2 + 0.1
         return val
+
+
+class EquilibriumChemistry(Chemistry):
+    def __init__(self, params: Dict):
+        super().__init__(params)
+        self.component_b_variable = "B"
+        self.b_parameter_key = "chemistry_parameters_b"
+        # Stoichiometric constant
+        self._beta = self.params.get("beta", 1)
+        self._equilibrium_constant = self.params.get("equilibrium_constant", .25)
+
+    def _initial_condition(self):
+        """Initial condition for all primary variables and the parameter representing the
+         secondary flux variable (needs to be present for first call to upwind discretization).
+        """
+        # Pressure, a, b and flux for a-parameter
+        super()._initial_condition()
+        # add b and flux for b-parameter
+        for g, d in self.gb:
+            state = {self.component_b_variable: self._initial_b(g)}
+            d[pp.STATE].update(state)
+            d[pp.STATE][pp.ITERATE].update(state.copy())
+            pp.initialize_data(g, d, self.b_parameter_key, {"darcy_flux": np.zeros(g.num_faces)})
+
+    def _assign_variables(self) -> None:
+        """
+        Assign primary variables to the nodes and edges of the grid bucket.
+        """
+        super()._assign_variables()
+        # First for the nodes
+        for g, d in self.gb:
+            d[pp.PRIMARY_VARIABLES].update({self.component_b_variable: {"cells": 1}})
+
+    def _create_ad_variables(self) -> None:
+        """Create the merged variables for potential and mortar flux"""
+        super()._create_ad_variables()
+        grid_list = [g for g, _ in self.gb.nodes()]
+        self._ad.component_b = self._eq_manager.merge_variables(
+            [(g, self.component_b_variable) for g in grid_list]
+        )
+
+    def _assign_equations(self) -> None:
+        """Define equations through discretizations.
+
+        Assigns a Laplace/Darcy problem discretized using Mpfa on all subdomains with
+        Neumann conditions on all internal boundaries. On edges of co-dimension one,
+        interface fluxes are related to higher- and lower-dimensional pressures using
+        the RobinCoupling.
+
+        Gravity is included, but may be set to 0 through assignment of the vector_source
+        parameter.
+        """
+        NonlinearIncompressibleFlow._assign_equations(self)
+
+        subdomain_transport_equation_a: pp.ad.Operator = self._subdomain_transport_equation(self.grid_list,
+                                                                                            component="a")
+        subdomain_transport_equation_b: pp.ad.Operator = self._subdomain_transport_equation(self.grid_list,
+                                                                                            component="b")
+        equilibrium_equation: pp.ad.Operator = self._equilibrium_equation(self.grid_list)
+        # Assign equations to manager
+        self._eq_manager.name_and_assign_equations(
+            {
+                "subdomain_transport_a": subdomain_transport_equation_a,
+                "subdomain_transport_b": subdomain_transport_equation_b,
+                "equilibrium": equilibrium_equation,
+            },
+        )
+
+    def _equilibrium_equation(self, subdomains: List[pp.Grid]) -> pp.ad.Operator:
+        """Mass balance equation for slightly compressible flow in a deformable medium.
+
+
+        Parameters
+        ----------
+        subdomains : List[pp.Grid]
+            Subdomains on which the equation is defined.
+
+        Returns
+        -------
+        eq : pp.ad.Operator
+            The equation on AD form.
+
+        """
+        equilibrium_constant = pp.ad.ParameterArray(
+            param_keyword=self.c_parameter_key,
+            array_keyword="equilibrium_constant",
+            grids=subdomains,
+        )
+        log = pp.ad.Function(pp.ad.functions.log, "logarithm")
+        eq: pp.ad.Operator = log(equilibrium_constant) - self._alpha * log(self._ad.component_a) - self._beta * log(
+            self._ad.component_b)
+
+        return eq
+
+    def before_newton_iteration(self):
+        super().before_newton_iteration()
+        self._ad.dummy_upwind_for_discretization_b.discretize(self.gb)
+
+    def compute_fluxes(self) -> None:
+        super().compute_fluxes()
+        # Copy to b parameters
+        for _, d in self.gb:
+            d[pp.PARAMETERS][self.b_parameter_key]["darcy_flux"] = d[pp.PARAMETERS][self.a_parameter_key]["darcy_flux"]
+
+    def _export_names(self):
+        return super()._export_names() + [self.component_b_variable]
--- utility_functions.py
+++ utility_functions.py
@@ -24,7 +24,7 @@ def plot_convergence(m_nonlin, m_lin, color=None, plot_errors=False, use_fn_labe
     if not use_fn_label:
         label = "non-diff"
     if model_type != "nonlinear":
-        plt.semilogy(iterations, m_lin._residuals, label=label, ls=":", color=color, marker="+")
+        plt.semilogy(iterations, m_lin._residuals, label=label, ls="-", color=color, marker="+")
     ax = plt.gca()
     ax.set_xlabel("Iteration")
     ax.set_ylabel("Residual norm")
@@ -135,6 +135,89 @@ def run_linear_and_nonlinear(params: Dict, model_class, plot_color=None, use_fn_
     plot_convergence(
         nonlinear_model, linear_model, plot_errors=False, color=plot_color, use_fn_label=use_fn_label
     )
+    # for m in ["linear", "nonlinear"]:
+    #     fname = "images/iterations/permeabilities_" + nonlinear_model.params["plotting_file_name"] + m
+    #     plot_line_solutions(nonlinear_model, linear_model, model_type=m, fname=fname)
     return linear_model, nonlinear_model
 
+
+def extract_line_solutions(model):
+    if not hasattr(model, "iteration_permeability"):
+        model.iteration_permeability = []
+        model.iteration_pressure = []
+        model.iteration_c = []
+    for g, d in model.gb:
+        cell_inds = np.logical_and(np.isclose(g.cell_centers[2], 0.5), np.isclose(g.cell_centers[1], 0.5))
+        if np.any(cell_inds):
+            model.iteration_permeability.append(model._permeability(g)[cell_inds])
+            if hasattr(model, "variable"):
+                model.iteration_pressure.append(d[pp.STATE][pp.ITERATE][model.variable][cell_inds])
+            else:
+                model.iteration_pressure.append(d[pp.STATE][pp.ITERATE][model.scalar_variable][cell_inds])
+            if hasattr(model, "variable"):
+                model.iteration_c.append(d[pp.STATE][pp.ITERATE][model.component_c_variable][cell_inds])
+
+def plot_line_solutions(m_nonlin, m_lin, color=None, model_type="both", fname=None):
+    # if use_fn_label:
+    #     label = m_lin.params["file_name"]
+    # else:
+    from_iteration = 0
+    vals_nonl = m_nonlin.iteration_pressure[from_iteration:]
+    vals_lin = m_lin.iteration_pressure[from_iteration:]
+    difference = True
+    for g, d in m_lin.gb:
+        cell_inds = np.logical_and(np.isclose(g.cell_centers[2], 0.5), np.isclose(g.cell_centers[1], 0.5))
+        x = g.cell_centers[0, cell_inds]
+
+    c_min =  np.inf
+    c_max = -np.inf
+    for i, perms in enumerate(vals_lin):
+        if difference:
+            c_min = min(c_min, np.min(perms - vals_lin[-1]))
+            c_max = max(c_max, np.max(perms- vals_lin[-1]))
+    if model_type != "linear":
+        for i, perms in enumerate(vals_nonl):
+            plt.figure()
+            if difference:
+                plt.plot(
+                    x, perms - vals_nonl[-1], label="Iteration " + str(i), ls="--", color=color, marker="x",
+                )
+            else:
+                plt.plot(
+                    x, perms, label="Iteration "+str(i), ls="--", color=color, marker="x",
+                )
+                plt.plot(x, vals_nonl[-1], label="Converged", ls=":", color=color, marker="x")
+
+            ax = plt.gca()
+            ax.set_xlabel("x coordinate")
+            ax.set_ylabel("K")
+            plt.ylim(c_min, c_max)
+            # Indicate convergence order
+            plt.legend(title=m_lin.params.get("legend_title", None), loc='upper right')
+            #if fname is None:
+            fname = "images/iterations/permeabilities_nonlinear" + m_nonlin.params["plotting_file_name"]
+            plt.savefig(fname=fname+str(i))
+    # if not use_fn_label:
+    if model_type != "nonlinear":
+
+        for i, perms in enumerate(vals_lin):
+            plt.figure()
+            if difference:
+                plt.plot(
+                    x, perms - vals_lin[-1], label="Iteration " + str(i), ls="--", color=color, marker="x",
+                )
+            else:
+                plt.plot(x, perms, label="Iteration "+str(i), ls="-", color=color, marker="+")
+                plt.plot(x, vals_lin[-1], label="Converged", ls=":", color=color, marker="x")
+            ax = plt.gca()
+            ax.set_xlabel("x coordinate")
+            ax.set_ylabel("K")
+            plt.ylim(c_min, c_max)
+
+            # Indicate convergence order
+            plt.legend(title=m_lin.params.get("legend_title", None), loc='upper right')
+            # if fname is None:
+            fname = "images/iterations/permeabilities_linear" + m_nonlin.params["plotting_file_name"]
+            plt.savefig(fname=fname+str(i))
 # Profiling: -B -m cProfile -o OutputFileName
+
